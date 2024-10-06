f6f7233224816f1e669b90d1739315e21fff04f1
DaYuan <dayuan.code@gmail.com>
2024-10-06 10:42:52 +0800

    fix email

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,8 +5,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  if (!process.env.RESEND_FROM_EMAIL) return;
   await resend.emails.send({
-    from: "mail@.asharib.xyz",
+    from: process.env.RESEND_FROM_EMAIL,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -15,9 +16,9 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
-
+  if (!process.env.RESEND_FROM_EMAIL) return;
   await resend.emails.send({
-    from: "mail@asharib.xyz",
+    from: process.env.RESEND_FROM_EMAIL,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -26,11 +27,22 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
-
+  if (!process.env.RESEND_FROM_EMAIL) return;
   await resend.emails.send({
-    from: "mail@asharib.xyz",
+    from: process.env.RESEND_FROM_EMAIL,
     to: email,
     subject: "Confirm your email",
+    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
+  });
+};
+
+export const sendLoginEmail = async (email: string, token: string) => {
+  const confirmLink = `${domain}/auth/login-token-verification?token=${token}`;
+  if (!process.env.RESEND_FROM_EMAIL) return;
+  await resend.emails.send({
+    from: process.env.RESEND_FROM_EMAIL,
+    to: email,
+    subject: "Please Click the Magic Link to Login your SEE2LINK ",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
 };
